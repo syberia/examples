5258f78055366761cb3b122de3b77784e485edea
Kirill Sevastyanenko <kirillseva@gmail.com>
2015-09-14 12:43:06 -0500

    drop sparse years

--- models/dev/uci/msd/msd.R
+++ models/dev/uci/msd/msd.R
@@ -10,12 +10,13 @@ list(
     ,"Rename timbre average vars"    = list( renamer, setNames(paste0('timbre_average_', 1:12), paste0('X', 2:13)))
     ,"Rename timbre covariance vars" = list( renamer, setNames(paste0('timbre_cov_', 1:78), paste0('X', 14:91)))
     ,"Select training rows"          = list( select_rows ~ NULL, 1:TRAIN_CUTOFF)
+    ,"Drop sparse years"             = list( select_rows ~ NULL, function(df) { bad_factors <- as.numeric(names(which(table(as.factor(df$dep_var)) < 5))); !df$dep_var %in% bad_factors}, whole = TRUE)
     ,"Set year as factor"            = list( column_transformation(function(x) as.factor(as.character(x))), c('dep_var'))
   ),
 
   model = list('gbm'
     , distribution        = 'multinomial'
-    , number_of_trees     = 6000
+    , number_of_trees     = 3000
     , shrinkage_factor    = 0.005
     , depth               = 5
     , min_observations    = 6
