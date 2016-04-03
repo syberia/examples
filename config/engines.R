25473c0fa69beab87f679b4a271b459423f1b859
Peter Hurford <peter@peterhurford.com>
2016-04-02 20:48:43 -0500

    use repo now that it is merged in

--- config/engines.R
+++ config/engines.R
@@ -1,4 +1,3 @@
-#engine("modeling", type = "github", repo = "robertzk/modeling.sy", mount = TRUE)
 engine("survey", type = "github", repo = "peterhurford/survey.sy", mount = TRUE)
-engine("modeling", type = "local", path = "~/dev/modeling.sy", mount = TRUE)
+engine("modeling", type = "github", repo = "robertzk/modeling.sy", mount = TRUE)
 
