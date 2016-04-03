6482c6dc8181efdd94d347afeffb9a6e888896bf
Peter Hurford <peter@peterhurford.com>
2016-04-02 21:01:21 -0500

    Use correct repo location

--- config/engines.R
+++ config/engines.R
@@ -1,3 +1,3 @@
 engine("survey", type = "github", repo = "peterhurford/survey.sy", mount = TRUE)
-engine("modeling", type = "github", repo = "robertzk/modeling.sy", mount = TRUE)
+engine("modeling", type = "github", repo = "syberia/modeling.sy", mount = TRUE)
 
