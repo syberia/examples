aacc556d14e4003e7f3fd0332a6f5486d49a03c3
Peter Hurford <peter@peterhurford.com>
2016-04-27 22:19:42 -0500

    Fix adapter

--- lib/adapters/url.R
+++ lib/adapters/url.R
@@ -1,5 +1,5 @@
 read <- function(name) {
-  readr::read_csv(text = RCurl::getURL(name))
+  utils::read.csv(text = RCurl::getURL(name))
 }
 
 write <- function(df) stop('Cannot write to a URL, aborting')
